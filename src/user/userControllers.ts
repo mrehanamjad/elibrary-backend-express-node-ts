036c8b568e9424e2bb4cfbbaf2d0bf384bd154d4
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-04-30 20:01:49 +0500

    - add hash password - improve valodation and error handeling

--- src/user/userControllers.ts
+++ src/user/userControllers.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // steps:
@@ -7,16 +9,29 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     // 2. Process (logic)
     // 3. Response
 
-    const {name, email, password} = req.body;
-
-
+    const { name, email, password } = req.body;
 
     // validation:
-    // module express validater can be se for complex validation
-    if(!name || !email || !password) {
-        const error = createHttpError(400, "All fields are required")
-        return next(error)
+    // module express validater can be used for complex validation
+    if (!name || !email || !password) {
+        const error = createHttpError(400, "All fields are required");
+        return next(error);
     }
+
+    try {
+        const user = await userModel.create({ name, email, password });
+        if(user) {
+            const error = createHttpError(400, "User with thos email already exists");
+            return next(error);
+        }
+
+    } catch (error) {
+        return next(createHttpError(500, "Error while getting user"))
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    
 
     res.json({ message: "User Registered Successfully" });
 };
