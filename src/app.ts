a39ee35bdb16480c2e849d64055951187b1c678e
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-04-30 18:54:33 +0500

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { HttpError } from "http-errors";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -8,11 +9,19 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to the  Elib apis" });
 });
 
-
+// Registering the router
+app.use("/api/users", userRouter);
 
 //  in express middleware is just a function (having parameter req, res, next), that runs before the route handler, and can be used to modify the request and response objectsfore the route handler, and can be used to modify the request and response objects
 // Global error handler is a special construct/funtion (having 4 parameter err, req, res, next), that runs when an error occurs in the application, and can be used to handle the error and send a response to the client
 // Global error handler , should be in the last
-app.use(globalErrorHandler as (err: HttpError, req: Request, res: Response, next: NextFunction) => void);
+app.use(
+    globalErrorHandler as (
+        err: HttpError,
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => void
+);
 
 export default app;
