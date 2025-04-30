932e7a71d0aec6cc451c9b34e95705391bb375b3
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-04-30 12:08:30 +0500

    add eslint

---
+++ server.ts
@@ -0,0 +1,18 @@
+console.log("Server is running...");
+
+const userObj = {
+  name: "Rehan",
+  age: 33,
+  phone: 927937293743,
+  email: "rehan@gmail.com",
+  address: {
+    street: "Mumbai",
+    pincode: 400012,
+    city: "Mumbai",
+    state: "Maharashtra",
+    country: "India"
+  }
+};
+
+
+console.log(userObj)
