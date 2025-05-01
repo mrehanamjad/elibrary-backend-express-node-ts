cd38dd2b707c67afb48769c29dd7dd1925274910
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-05-01 20:07:28 +0500

    add endpoint to retrieve a specific book by ID

--- src/book/bookControllers.ts
+++ src/book/bookControllers.ts
@@ -208,6 +208,22 @@ const listBooks = async (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
+const getSingleBook = async (req: Request, res: Response, next: NextFunction) => {
+    
+    try {
 
+        const bookId = req.params.bookId;
 
-export { createBook, updateBook, listBooks };
+        const book = await bookModel.findOne({ _id: bookId }).populate("author", "name");
+
+        if (!book) {
+            return next(createHttpError(404, "Book not found"));
+        }
+
+        res.status(200).json(book);
+    } catch (error) {
+        return next(createHttpError(500, "Error while getting a book"));
+    }
+}
+
+export { createBook, updateBook, listBooks, getSingleBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook, listBooks, updateBook } from "./bookControllers";
+import { createBook, getSingleBook, listBooks, updateBook } from "./bookControllers";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -34,5 +34,7 @@ bookRouter.patch(
 );
 
 bookRouter.get("/",listBooks)
+
+bookRouter.get("/:bookId", getSingleBook);
 
 export default bookRouter;
