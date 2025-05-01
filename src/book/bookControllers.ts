b97dd8c5aad14743f346bf88ac39ef8a92c9028b
M.Rehan Amjad <rehanamjad814@gmail.com>
2025-05-01 19:48:49 +0500

    add update book

--- src/book/bookControllers.ts
+++ src/book/bookControllers.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 import createHttpError from "http-errors";
 import bookModel from "./bookModel";
 import fs from "fs";
+import { AuthRequest } from "../middlewares/authenticate";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     const { title, genre, description } = req.body;
@@ -60,12 +61,14 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         console.log("image upload result", uploadResult);
         console.log("book upload result", bookFileUploadResult);
 
+        const _req = req as AuthRequest;
+
         try {
             const newBook = await bookModel.create({
                 title,
                 genre,
                 description,
-                author: "68134f28ecdca76f1d55417e",
+                author: _req.userId,
                 coverImage: uploadResult.secure_url,
                 file: bookFileUploadResult.secure_url,
             });
@@ -90,4 +93,110 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     }
 };
 
-export { createBook };
+const updateBook = async (req: Request, res: Response, next: NextFunction) => {
+    const { title, genre, description } = req.body;
+
+    if (!title || !genre || !description) {
+        return next(
+            createHttpError(400, "Title, genre and description are required")
+        );
+    }
+
+    const bookId = req.params.bookId;
+
+    if (!bookId) {
+        return next(createHttpError(400, "Book id is required"));
+    }
+
+    const book = await bookModel.findOne({ _id: bookId });
+
+    if (!book) {
+        return next(createHttpError(404, "Book not found"));
+    }
+
+    const _req = req as AuthRequest;
+    if (book.author.toString() !== _req.userId) {
+        return next(
+            createHttpError(403, "You are not authorized to update this book")
+        );
+    }
+
+    const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    let completeCoverImage = "";
+    if (files.coverImage) {
+        try {
+            const fileName = files.coverImage[0].filename;
+            const filePath = path.resolve(
+                __dirname,
+                "../../public/data/uploads",
+                fileName
+            );
+            const coverImageMineType = files.coverImage[0].mimetype
+                .split("/")
+                .at(-1);
+            completeCoverImage = fileName;
+            const uploadResult = await cloudinary.uploader.upload(filePath, {
+                filename_override: fileName,
+                folder: "book-covers",
+                formate: coverImageMineType,
+            });
+
+            completeCoverImage = uploadResult.secure_url;
+            await fs.promises.unlink(filePath);
+        } catch (error) {
+            return next(createHttpError(500, "Error uploading cover image"));
+        }
+    }
+
+    let completeFile = "";
+    if (files.file) {
+        try {
+            const bookFileName = files.file[0].filename;
+            const bookFilePath = path.resolve(
+                __dirname,
+                "../../public/data/uploads",
+                bookFileName
+            );
+            completeFile = bookFileName;
+
+            const bookFileUploadResult = await cloudinary.uploader.upload(
+                bookFilePath,
+                {
+                    resource_type: "raw",
+                    filename_override: bookFileName,
+                    folder: "book-files",
+                    format: "pdf",
+                }
+            );
+
+            completeFile = bookFileUploadResult.secure_url;
+            await fs.promises.unlink(bookFilePath);
+        } catch (error) {
+            return next(createHttpError(500, "Error uploading book pdf"));
+        }
+    }
+
+    try {
+        const updatedBook = await bookModel.findOneAndUpdate(
+            {
+                _id: bookId,
+            },
+            {
+                title,
+                genre,
+                description,
+                coverImage: completeCoverImage || book.coverImage,
+                file: completeFile || book.file,
+            },
+            {
+                new: true,
+            }
+        );
+
+        res.status(200).json(updatedBook);
+    } catch (error) {
+        return next(createHttpError(500, "Error updating book"));
+    }
+};
+
+export { createBook, updateBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook } from "./bookControllers";
+import { createBook, updateBook } from "./bookControllers";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -21,6 +21,16 @@ bookRouter.post(
         { name: "file", maxCount: 1 },
     ]),
     createBook
+);
+
+bookRouter.patch(
+    "/:bookId",
+    authenticate,
+    uplaod.fields([
+        { name: "coverImage", maxCount: 1 },
+        { name: "file", maxCount: 1 },
+    ]),
+    updateBook
 );
 
 export default bookRouter;
